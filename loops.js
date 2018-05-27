d8095dfe60fb28e42a93d6c04a7413b6e7946f79
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-27 18:30:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
 }
   return "done"
 }
+
+function maybeTrue() {
+    return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+}
